227fe6736a053b2383918dcaea2e354e39f2a802
ThaoLe98 <thaole4416@gmail.com>
2020-06-29 22:25:52 +0700

    Form Tag Helpers: Working with Select and Option Elements Populating with asp-items

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -16,6 +17,7 @@ namespace WebApp.Controllers
 
         public async Task<IActionResult> Index(long id = 1)
         {
+            ViewBag.Categories= new SelectList(context.Categories, "CategoryId", "Name");
             return View("Form", await context.Products.FindAsync(id));
         }
 
