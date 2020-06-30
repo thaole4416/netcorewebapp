298fcd9b7bf2a60302cc32c2976ebc48d4a768e1
ThaoLe98 <thaole4416@gmail.com>
2020-06-30 10:40:25 +0700

    Model Validation: Configuring the Default Validation Error Messages

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +29,8 @@ namespace WebApp
             services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddSingleton<CitiesData>();
             services.Configure<AntiforgeryOptions>(opts => { opts.HeaderName = "X-XSRF-TOKEN"; });
+            services.Configure<MvcOptions>(opts =>
+                opts.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(value => "Please enter a value"));
         }
 
         public void Configure(IApplicationBuilder app, DataContext context, IAntiforgery antiforgery)
@@ -42,6 +45,7 @@ namespace WebApp
                     context.Response.Cookies.Append("XSRF-TOKEN", antiforgery.GetAndStoreTokens(context).RequestToken,
                         new CookieOptions {HttpOnly = false});
                 }
+
                 await next();
             });
             app.UseEndpoints(endpoints =>
