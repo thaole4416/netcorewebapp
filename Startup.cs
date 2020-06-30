665eed4d59a962e946313d838ab52e0b178d0d61
ThaoLe98 <thaole4416@gmail.com>
2020-06-30 18:19:28 +0700

    Using Filters: Understanding Filter Order

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,11 @@ namespace WebApp
             services.Configure<MvcOptions>(opts =>
                 opts.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(value => "Please enter a value"));
             services.AddScoped<GuidResponseAttribute>();
-            services.Configure<MvcOptions>(opts => opts.Filters.Add<HttpsOnlyAttribute>()); 
+            services.Configure<MvcOptions>(opts =>
+            {
+                opts.Filters.Add<HttpsOnlyAttribute>();
+                opts.Filters.Add(new MessageAttribute("This is the globally-scoped filter"));
+            });
         }
 
         public void Configure(IApplicationBuilder app, DataContext context, IAntiforgery antiforgery)
