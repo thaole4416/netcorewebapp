2b9191b87458088febf2f1af5e81974d80331447
ThaoLe98 <thaole4416@gmail.com>
2020-06-30 17:09:26 +0700

    Using Filters: Understanding Resource Filters

---
+++ Filters/SimpleCacheAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+
+namespace WebApp.Filters
+{
+    public class SimpleCacheAttribute : Attribute, IResourceFilter
+    {
+        private Dictionary<PathString, IActionResult> CachedResponses = new Dictionary<PathString, IActionResult>();
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            PathString path = context.HttpContext.Request.Path;
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(CachedResponses);
+            Console.WriteLine(context.Result);
+            Console.WriteLine(context.HttpContext.Request.Path);
+            Console.WriteLine("------------------------------------------------------");
+            if (CachedResponses.ContainsKey(path))
+            {
+                context.Result = CachedResponses[path];
+                CachedResponses.Remove(path);
+            }
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            CachedResponses.Add(context.HttpContext.Request.Path, context.Result);
+        }
+    }
+}
