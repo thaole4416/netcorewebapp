6774e926a7d2e9d37390a27b9cd24b1638cb2a43
ThaoLe98 <thaole4416@gmail.com>
2020-06-30 17:42:58 +0700

    Using Filters: Creating a Result Filter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 namespace WebApp.Controllers
 {
     [HttpsOnly]
+    [ResultDiagnostics]
     public class HomeController : Controller
     {
         public IActionResult Index()
