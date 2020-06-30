4207bb1575c7345ea12e22d34ff0866db3526954
ThaoLe98 <thaole4416@gmail.com>
2020-06-30 17:53:11 +0700

    Using Filters: Understanding Exception Filters

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using WebApp.Filters;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -17,7 +18,8 @@ namespace WebApp.Controllers
         public IActionResult Secure()
         {
             return View("Message", "This is the Secure action on the Home controller");
-        } 
+        }
+
         //[ChangeArg]
         public IActionResult Messages(string message1, string message2 = "None")
         {
@@ -29,6 +31,23 @@ namespace WebApp.Controllers
             if (context.ActionArguments.ContainsKey("message1"))
             {
                 context.ActionArguments["message1"] = "New message";
+            }
+        }
+
+        [RangeException]
+        public ViewResult GenerateException(int? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            else if (id > 10)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            else
+            {
+                return View("Message", $"The value is {id}");
             }
         }
     }
